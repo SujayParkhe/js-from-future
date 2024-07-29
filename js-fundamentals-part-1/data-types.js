2d58ee456b7c333357fe3d7c6b747c76c5bfd5f1
sujay parkhe <sparkhe852@gmail.com>
2024-07-29 13:15:37 +0530

    learning data-types

---
+++ js-fundamentals-part-1/data-types.js
@@ -0,0 +1,17 @@
+// Number
+let age = 29;
+
+// String
+let firstName = 'Sujay';
+
+// Boolean
+let isJavaScriptFun = true;
+console.log(typeof isJavaScriptFun);
+
+// Undefined
+let year;
+console.log(typeof year);
+
+// Null
+let person = null;
+console.log(typeof person);
