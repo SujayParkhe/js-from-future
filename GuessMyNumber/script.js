c3abaf83d636c7d30b4fd728cb902a707bfe3576
Sujay Parkhe <sparkhe952@gmail.com>
2024-10-10 12:00:14 +0530

    logic

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -7,13 +7,28 @@
 // const guess = document.querySelector(".guess").value;
 // console.log(guess);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let defaultScore = 20;
+document.querySelector(".number").textContent = secretNumber;
+const score = document.querySelector(".score");
+
 const guessNumber = () => {
   const guess = Number(document.querySelector(".guess").value);
   const message = document.querySelector(".message");
-  console.log(guess);
+  console.log(score);
 
   if (!guess) {
     message.textContent = "No Number";
+  } else if (guess === secretNumber) {
+    message.textContent = "Correct number"
+  } else if (guess > secretNumber) {
+    message.textContent = "Too High!!"
+    defaultScore = defaultScore - 1;
+    score.textContent = defaultScore;
+  } else if (guess < secretNumber) {
+    message.textContent = "Too Low!!"
+    defaultScore = defaultScore - 1;
+    score.textContent = defaultScore;
   }
 };
 
